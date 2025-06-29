dcd9a352feccee34551220986324292f25c726b6
au9u5t <1241407552@qq.com>
2025-06-29 20:31:20 +0800

    refactor: 重构了项目结构

--- run_with_action.sh
+++ run_with_action.sh
@@ -6,19 +6,24 @@ CODE_TYPE="$2"
 OUTPUT_REPORT="$3"
 TARGET_CODE="$4"
 
+
+# 移动递归 rule 到根目录 /rule
+mv /app/rule /
+
+
 # 确定使用的规则文件
 case "$CODE_TYPE" in
     "trpc")
-        RULE_FILE="rule/find_trpc_api.yaml"
+        RULE_FILE="/rule/find_trpc_api.yaml"
         ;;
     "gin")
-        RULE_FILE="rule/find_gin_api.yaml"
+        RULE_FILE="/rule/find_gin_api.yaml"
         ;;
     "go_swagger")
-      RULE_FILE="rule/find_go_swagger_api.yaml"
+      RULE_FILE="/rule/find_go_swagger_api.yaml"
       ;;
     *)
-        RULE_FILE="rule/find_go_swagger_api.yaml"
+        RULE_FILE="/rule/find_go_swagger_api.yaml"
         ;;
 esac
 
@@ -42,6 +47,9 @@ ls -al
 echo "/app 目录"
 ls -al /app
 
+# 制作findCode 到 全局可调用的地方
+ln -s /app/FindCode /usr/local/bin/FindCode
+
 
 # 执行FindCode扫描
 echo "执行FindCode扫描..."
@@ -50,7 +58,7 @@ echo "代码类型: $CODE_TYPE"
 echo "使用规则文件: $RULE_FILE"
 echo "输出报告路径: $OUTPUT_REPORT"
 
-./app/FindCode -l "$TARGET_PATH" -r "$RULE_FILE" -o "$OUTPUT_REPORT" -go_target "$TARGET_CODE"
+FindCode -l "$TARGET_PATH" -r "$RULE_FILE" -o "$OUTPUT_REPORT" -go_target "$TARGET_CODE" -c "/app/etc/config.yaml"
 
 # 检查执行状态
 if [ $? -eq 0 ]; then
